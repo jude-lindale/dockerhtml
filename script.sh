73a7d9c1c7c4a83e1aaab3a3117f237d53388f29
jude-lindale <70959569+jude-lindale@users.noreply.github.com>
2022-09-28 19:07:14 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-$user = mohammed
+user = jude
 
 echo $'\e[1;32m'"Downloading files..."$'\e[0m'
 echo ""
 ## DOWNLOADING AND CREATING DOCKER COMPOSE FILES ##
-mkdir /home/$user/docker-db
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/docker-compose-db.yml -o /home/$user/docker-db/docker-compose.yml
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/Dockerfile -o /home/$user/docker-db/Dockerfile
-mkdir /home/$user/docker-web
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/docker-compose-web.yml -o /home/$user/docker-web/docker-compose.yml
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/index.php -o /home/$user/docker-web/index.php
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/server.php -o /home/$user/docker-web/server.php
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/errors.php -o /home/$user/docker-web/errors.php
+mkdir /home/user/docker-db
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/docker-compose-db.yml -o /home/user/docker-db/docker-compose.yml
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/Dockerfile -o /home/user/docker-db/Dockerfile
+mkdir /home/user/docker-web
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/docker-compose-web.yml -o /home/user/docker-web/docker-compose.yml
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/index.php -o /home/user/docker-web/index.php
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/server.php -o /home/user/docker-web/server.php
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/errors.php -o /home/user/docker-web/errors.php
 
 echo ""
 echo $'\e[1;32m'"Starting docker!"$'\e[0m'
 echo ""
-cd /home/$user/docker-db
+cd /home/user/docker-db
 docker-compose up -d
 sleep 7
-cd /home/$user/docker-web
+cd /home/user/docker-web
 docker-compose up -d
 sleep 7
 
